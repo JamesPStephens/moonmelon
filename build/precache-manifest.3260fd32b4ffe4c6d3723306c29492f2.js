bf072cde197befd319aec2e768ce55864b5d9672
James Stephens <jamesstephens@Jamess-MacBook-Pro.local>
2020-03-03 15:47:21 +0000

    Push build.

---
+++ build/precache-manifest.3260fd32b4ffe4c6d3723306c29492f2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d86d204e93a262f4c76f1de864165b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d53d9a6a346f3366c207",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1acdeb28cf97c14ff807",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d53d9a6a346f3366c207",
+    "url": "/static/js/2.dc445428.chunk.js"
+  },
+  {
+    "revision": "d48f557b335fe40c661607e7b46704c8",
+    "url": "/static/js/2.dc445428.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1acdeb28cf97c14ff807",
+    "url": "/static/js/main.22ba58b1.chunk.js"
+  },
+  {
+    "revision": "a30eb7c744a9c0a92399",
+    "url": "/static/js/runtime-main.c196efb1.js"
+  },
+  {
+    "revision": "0f3514a45d51f95167e5fe8b6a03bb60",
+    "url": "/static/media/Download_on_the_App_Store_Badge_US-UK_RGB_wht_092917.0f3514a4.svg"
+  },
+  {
+    "revision": "a967ea2ad35ed4af16e3b71da3423535",
+    "url": "/static/media/Frame 1.a967ea2a.svg"
+  },
+  {
+    "revision": "db841a6fba920cdd4453d78a08219e5c",
+    "url": "/static/media/Frame 2.db841a6f.svg"
+  },
+  {
+    "revision": "95793bf1b2808de5d73a9c5b20a9d526",
+    "url": "/static/media/video.95793bf1.mp4"
+  }
+]);
